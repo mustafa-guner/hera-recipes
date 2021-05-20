c7d7ad944b08d4d63dade6723b992a085433a599
slex1one-Musdy <legend123412@outlook.com>
2021-05-20 03:25:03 +0300

    Updated UI Menu added

--- public/js/ScrollTriggerXAxis.js
+++ public/js/ScrollTriggerXAxis.js
@@ -3,15 +3,14 @@ gsap.registerPlugin(ScrollTrigger);
 let sections = gsap.utils.toArray(".panel");
 
 gsap.to(sections, {
-  xPercent: -200 * (sections.length - 1),
+  xPercent: -100 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
-    trigger: ".main-content",
+    trigger: ".container",
     pin: true,
     scrub: 1,
     snap: 1 / (sections.length - 1),
     // base vertical scrolling on how wide the container is so it feels more natural.
-    end: () => "+=" + document.querySelector(".main-content").offsetWidth
+    end: () => "+=" + document.querySelector(".container").offsetWidth
   }
 });
-
